4264cecbe162591b3422234da6f1f7161586daa0
Tedo74 <teddy2@abv.bg>
2017-09-26 21:17:34 +0300

    Regex

---
+++ MatchFullName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.MatchFullName
+{
+    class MatchFullName
+    {
+        static void Main()
+        {
+            Regex pattern = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+            string fullName = Console.ReadLine();
+            MatchCollection matchedNames = pattern.Matches(fullName);
+            foreach (Match name in matchedNames)
+            {
+                Console.Write(name.Value+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
