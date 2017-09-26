ea77c58bbe31a96052098021a701952a2f497095
Tedo74 <teddy2@abv.bg>
2017-09-27 00:59:25 +0300

    ExamPreparation

---
+++ ExamPreparation/13.NetherRealms/NetherRealms.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _13.NetherRealms
+{
+    class Demon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public double Damage { get; set; }
+
+        public Demon(string name, int health, double damage)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Damage = damage;
+        }
+    }
+    class NetherRealms
+    {
+        static void Main(string[] args)
+        {
+            List<Demon> demons = new List<Demon>();
+            string healthPattern = @"[^0-9\+\-*\/\.]+";
+            string damageNumbersPattern = @"([\-\+]?[\d]+[\.]?\d*)";
+            string damageMultyplayerPattern = @"[\*\/]";
+            string[] demonNames = Console.ReadLine()
+                .Split(new char[] {',', ' '}, StringSplitOptions.RemoveEmptyEntries);
+            Regex health = new Regex(healthPattern);
+            foreach (string demon in demonNames)
+            {
+                MatchCollection matches = health.Matches(demon);
+                StringBuilder sb = new StringBuilder();
+                foreach (Match match in matches)
+                {
+                    sb.Append(match.Value);
+                }
+                int daemonHealth = GetDemonHealth(sb.ToString());
+
+                Regex numbers = new Regex(damageNumbersPattern);
+                double damage = 0.0;
+                MatchCollection nums = numbers.Matches(demon);
+                foreach (Match n in nums)
+                {
+                    damage += double.Parse(n.Value);
+                }
+                Regex multyplayer = new Regex(damageMultyplayerPattern);
+                MatchCollection multyplayers = multyplayer.Matches(demon);
+                foreach (Match sign in multyplayers)
+                {
+                    if (sign.Value == "/" && damage != 0)
+                    {
+                        damage /= 2.0;
+                    }
+                    if (sign.Value == "*")
+                    {
+                        damage *= 2;
+                    }
+                }
+                Demon demoncho = new Demon(demon, daemonHealth, damage);
+                demons.Add(demoncho);
+            }
+            demons = demons.OrderBy(d => d.Name).ToList();
+            foreach (Demon dem in demons)
+            {
+                Console.WriteLine($"{dem.Name} - {dem.Health} health, {dem.Damage:F2} damage");
+            }
+        }
+
+        static int GetDemonHealth(string d)
+        {
+            int health = 0;
+            for (int i = 0; i < d.Length; i++)
+            {
+                health += d[i];
+            }
+            return health;
+        }
+    }
+}
---
+++ ExamPreparation/14.RoliTheCoder/RoliTheCoder.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.RoliTheCoder
+{
+    class Event
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public List<string> Participants { get; set; }
+
+        public Event(int id, string name)
+        {
+            this.EventId = id;
+            this.Name = name;
+            this.Participants = new List<string>();
+        }
+    }
+
+    class RoliTheCoder
+    {
+        public static void Main()
+        {
+            Dictionary<int, Event> allEvents = new Dictionary<int, Event>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Time for Code")
+                {
+                    break;
+                }
+                string[] eventInput = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (eventInput.Length < 2)
+                {
+                    continue;
+                }
+                int eventNumber = -1;
+                if (int.TryParse(eventInput[0], out eventNumber)
+                    && eventInput[1][0] == '#')
+                {
+                    string eventName = eventInput[1].Trim('#');
+                    Event e;
+                    if (allEvents.ContainsKey(eventNumber))
+                    {
+                        e = allEvents[eventNumber];
+                        if (e.Name != eventName || eventInput.Length <= 2)
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        e = new Event(eventNumber, eventName);
+                    }
+                    string[] participants = eventInput.Skip(2).ToArray();
+                    if (!participants.All(p => p.StartsWith("@")))
+                    {
+                        continue;
+                    }
+                    foreach (var participant in participants)
+                    {
+                        if (!e.Participants.Contains(participant))
+                        {
+                            e.Participants.Add(participant);
+                        }
+                    }
+                    allEvents[eventNumber] = e;
+                }
+            }
+            var orderedEvents = allEvents.OrderByDescending(e => e.Value.Participants.Count).ThenBy(e => e.Value.Name);
+            foreach (var ev in orderedEvents)
+            {
+                Console.WriteLine($"{ev.Value.Name} - {ev.Value.Participants.Count}");
+                if (ev.Value.Participants.Any())
+                {
+                    var ordered = ev.Value.Participants.OrderBy(e => e).ToList();
+                    foreach (var participant in ordered)
+                    {
+                        Console.WriteLine(participant);
+                    }
+                }
+            }
+        }
+    }
+}
