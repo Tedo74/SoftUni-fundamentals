a2d34280bde43069733eecdfb785bbd2e2bb1dea
Tedo74 <teddy2@abv.bg>
2017-09-26 21:29:59 +0300

    MatchPhoneNumber

---
+++ Regex/02.MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _02.MatchPhoneNumber
+{
+    class MatchPhoneNumber
+    {
+        static void Main()
+        {
+            string pattern = @"\+359([ -])2\1\d{3}\1\d{4}\b";
+            string phones = Console.ReadLine();
+            MatchCollection matchedPhones = Regex.Matches(phones, pattern);
+            string[] phonesInSofia = matchedPhones.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", phonesInSofia));
+        }
+    }
+}
