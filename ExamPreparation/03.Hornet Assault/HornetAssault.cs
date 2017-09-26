e9fb98dbfdcca0ba8f2a4f6e25c3d3fe6f721ba6
Tedo74 <teddy2@abv.bg>
2017-09-26 22:29:35 +0300

    ExamPreparation

---
+++ ExamPreparation/03.Hornet Assault/HornetAssault.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Hornet_Assault
+{
+    class HornetAssault
+    {
+        public static void Main()
+        {
+            List<long> beehives = Console.ReadLine().Split(' ').Select(long.Parse).ToList();
+            List<long> hornets = Console.ReadLine().Split(' ').Select(long.Parse).ToList();
+
+            for (int i = 0; i < beehives.Count; i++)
+            {
+                if (!hornets.Any())
+                {
+                    break;
+                }
+                long hornetsPower = hornets.Sum();
+
+                if (hornetsPower > beehives[i])
+                {
+                    beehives.RemoveAt(i);
+                    i--;
+                }
+                else if (hornetsPower < beehives[i])
+                {
+                    beehives[i] = beehives[i] - hornetsPower;
+                    hornets.RemoveAt(0);
+                }
+                else // hornets equals bees
+                {
+                    hornets.RemoveAt(0);
+                    beehives.RemoveAt(i);
+                    i--;
+                }
+            }
+            if (beehives.Any())
+            {
+                Console.WriteLine(string.Join(" ", beehives));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", hornets));
+            }
+        }
+    }
+}
