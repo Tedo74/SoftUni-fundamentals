fd0d57fae42dd8ce361ebe0c3bd0a3140d306bdc
Tedo74 <teddy2@abv.bg>
2017-09-27 16:47:12 +0300

    Upload

---
+++ ExamPreparation/83.Wormhole/Wormhole.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _83.Wormhole
+{
+    class Wormhole
+    {
+        static void Main(string[] args)
+        {
+            int[] space = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int count = 0;
+            int steps = 0;
+            while (true)
+            {
+                if (space[count] == 0)
+                {
+                    count++;
+                    steps++;
+                }
+                else
+                {
+                    int temp = count;
+                    count = space[count];
+                    space[temp] = 0;
+                }
+                if (count > space.Length -1)
+                {
+                   break; 
+                }
+            }
+            Console.WriteLine(steps);
+        }
+    }
+}
---
+++ ExamPreparation/84.WormsWorldParty/WWparty.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _84.WormsWorldParty
+{
+    class WWparty
+    {
+        static void Main(string[] args)
+        {
+            List<string> participants = new List<string>();
+            Dictionary<string, Dictionary<string, long>> teams = new Dictionary<string, Dictionary<string, long>>();
+            string input = Console.ReadLine();
+            while (input != "quit")
+            {
+                string[] wormData = input.Split(new string[] {" -> "}, StringSplitOptions.RemoveEmptyEntries);
+                string wormName = wormData[0];
+                string teamName = wormData[1];
+                int points =int.Parse(wormData[2]);
+                if (participants.Contains(wormName))
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+                else
+                {
+                    participants.Add(wormName);
+                    if (!teams.ContainsKey(teamName))
+                    {
+                        teams.Add(teamName, new Dictionary<string, long>());
+                    }
+                    if (!teams[teamName].ContainsKey(wormName))
+                    {
+                        teams[teamName].Add(wormName, 0);
+                    }
+                    teams[teamName][wormName] += points;
+                }
+                input = Console.ReadLine();
+            }
+            var orderedWorms = teams.OrderByDescending(w => w.Value.Sum(p => p.Value))
+                .ThenBy(w => w.Value.Count);
+            int cnt = 1;
+            foreach (var w in orderedWorms)
+            {
+                Console.WriteLine($"{cnt}. Team: {w.Key} - {w.Value.Sum(points => points.Value)}");
+                cnt++;
+                foreach (var worm in w.Value.OrderByDescending(n => n.Value))
+                {
+                    Console.WriteLine($"###{worm.Key} : {worm.Value}");
+                }
+            }
+        }
+    }
+}
