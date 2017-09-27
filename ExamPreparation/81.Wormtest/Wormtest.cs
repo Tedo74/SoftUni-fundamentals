0666c11e31184511a316a80c75bb28f5eb092ed1
Tedo74 <teddy2@abv.bg>
2017-09-27 11:59:06 +0300

    Upload

---
+++ ExamPreparation/81.Wormtest/Wormtest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _81.Wormtest
+{
+    class Wormtest
+    {
+        static void Main(string[] args)
+        {
+            int wormLengthInMeters = int.Parse(Console.ReadLine());
+            double wormWidth = double.Parse(Console.ReadLine());
+            int wormLengthCm = wormLengthInMeters * 100;
+            double reminder = wormLengthCm % wormWidth;
+            if (reminder != 0 && wormWidth != 0 && wormLengthCm != 0)
+            {
+                double result = Math.Round(wormLengthCm / wormWidth * 100.0, 2);
+                Console.WriteLine($"{result:F2}%");
+            }
+            else
+            {
+                double result = Math.Round(wormLengthCm * wormWidth, 2);
+                Console.WriteLine($"{result:F2}");
+            }
+        }
+    }
+}
---
+++ ExamPreparation/82.WormIpsum/WormIpsum.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _82.WormIpsum
+{
+    class WormIpsum
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex isValidSentence = new Regex(@"^[A-Z]+([^\.])*\.$");
+            while (input != "Worm Ipsum")
+            {
+                if (isValidSentence.IsMatch(input))
+                {
+                    string sentece = WormSentence(input);
+                    Console.WriteLine(sentece);
+                }
+                input = Console.ReadLine();
+            }
+        }
+
+        private static string WormSentence(string input)
+        {
+            string[] wordsInSentence = input.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < wordsInSentence.Length; i++)
+            {
+                string word = wordsInSentence[i];
+                string tempWord = new string(word.ToCharArray().Distinct().ToArray());
+                if (word != tempWord)
+                {
+                    Dictionary<char, int> wordChars = new Dictionary<char, int>();
+                    foreach (char c in word)
+                    {
+                        if (!wordChars.ContainsKey(c))
+                        {
+                            wordChars.Add(c, 0);
+                        }
+                        wordChars[c] += 1;
+                    }
+                    char max = wordChars.OrderByDescending(c => c.Value).First().Key;
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < word.Length; j++)
+                    {
+                        if (!char.IsPunctuation(word[j]))
+                        {
+                            sb.Append(max);
+                        }
+                        else
+                        {
+                            sb.Append(word[j]);
+                        }
+                    }
+                    wordsInSentence[i] = sb.ToString();
+                }
+            }
+            return string.Join(" ", wordsInSentence);
+        }
+    }
+}
