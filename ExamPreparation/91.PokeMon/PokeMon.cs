6ceeaa15a26ff83cb7b869bb43ee2b263f78ce07
Tedo74 <teddy2@abv.bg>
2017-09-30 18:52:22 +0300

    upload 91, 92

---
+++ ExamPreparation/91.PokeMon/PokeMon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _91.PokeMon
+{
+    class PokeMon
+    {
+        static void Main(string[] args)
+        {
+            int poketPower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustFactor = int.Parse(Console.ReadLine());
+            int reachedTargets = 0;
+            int halfPower = poketPower / 2;
+            while (poketPower >= distance)
+            {
+                poketPower = poketPower - distance;
+                reachedTargets++;
+                if (poketPower == halfPower && poketPower !=0 && exhaustFactor !=0)
+                {
+                    poketPower = poketPower / exhaustFactor;
+                }
+            }
+            Console.WriteLine(poketPower);
+            Console.WriteLine(reachedTargets);
+        }
+    }
+}
---
+++ ExamPreparation/92.PokemonDontGo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _92.PokemonDontGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int removedElementsTotalValue = 0;
+            while (numbers.Any())
+            {
+                int index = int.Parse(Console.ReadLine());
+                int value;
+                if (index >= 0 && index < numbers.Count)
+                {
+                    value = numbers[index];
+                    numbers.RemoveAt(index);
+                    removedElementsTotalValue += value;
+                    if (!numbers.Any())
+                    {
+                        break;
+                    }
+                    numbers = numbers.Select(n => ChangeValue(value, n)).ToList();
+                }
+                else if (index < 0)
+                {
+                    value = numbers[0];
+                    removedElementsTotalValue += value;
+                    numbers[0] = numbers[numbers.Count - 1];
+                    numbers = numbers.Select(n => ChangeValue(value, n)).ToList();
+                }
+                else
+                {
+                    value = numbers[numbers.Count - 1];
+                    removedElementsTotalValue += value;
+                    numbers[numbers.Count - 1] = numbers[0];
+                    numbers = numbers.Select(n => ChangeValue(value, n)).ToList();
+                }
+            }
+            Console.WriteLine(removedElementsTotalValue);
+        }
+
+        private static int ChangeValue(int value, int numbersValue)
+        {
+            if (numbersValue <= value)
+            {
+                numbersValue += value;
+            }
+            else
+            {
+                numbersValue -= value;
+            }
+            return numbersValue;
+        }
+    }
+}
