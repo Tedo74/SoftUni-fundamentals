92269f1ed9a6d28396d06b784d5065edc58cbcb5
Tedo74 <teddy2@abv.bg>
2017-10-17 16:58:20 +0300

    Arrays Exercises

---
+++ Arrays/01.LargestCommonEnd/LargestCommonEnd.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.LargestCommonEnd
+{
+    public class LargestCommonEnd
+    {
+        public static void Main()
+        {
+            string[] arr1 = Console.ReadLine().Split(' ');
+            string[] arr2 = Console.ReadLine().Split(' ');
+            int leftEquals = Equals(arr1, arr2);
+            Array.Reverse(arr1);
+            arr2 = arr2.Reverse().ToArray();
+            int rightEquals = Equals(arr1, arr2);
+            int max = Math.Max(leftEquals, rightEquals);
+            Console.WriteLine(max);
+        }
+
+        private static int Equals(string[] arr1, string[] arr2)
+        {
+            int firstArrLength = arr1.Length;
+            int secondArrLength = arr2.Length;
+            int n = Math.Min(firstArrLength, secondArrLength);
+            int longuestEquals = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr1[i] == arr2[i])
+                {
+                    longuestEquals++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return longuestEquals;
+        }
+    }
+}
---
+++ Arrays/02.RotateAndSum/RotateAndSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.RotateAndSum
+{
+    public class RotateAndSum
+    {
+        public static void Main()
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+            int[] sum = new int[arr.Length];
+            for (int i = 0; i < rotations; i++)
+            {
+                RotateArray(arr);
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    sum[j] = arr[j] + sum[j];
+                }
+            }
+            Console.WriteLine(string.Join(" ", sum));
+        }
+
+        public static void RotateArray(int[] arr)
+        {
+            int last = arr[arr.Length - 1];
+            for (int i = arr.Length - 2; i >= 0; i--)
+            {
+                arr[i + 1] = arr[i];
+            }
+            arr[0] = last;
+        }
+    }
+}
